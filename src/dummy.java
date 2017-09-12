cc3d5420412e188ead011e9c801d2cb3576bbd2b
otisLITS <otis01990@gmail.com>
2017-09-12 11:29:05 +0300

    Git check

---
+++ src/dummy.java
@@ -0,0 +1,8 @@
+public class dummy {
+    public static void main(String[] args){
+            String s ="Hello world";
+            System.out.println(s);
+            System.out.println("Git check");
+    }
+
+}
